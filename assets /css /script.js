5c368e6305c432e632b8c3380ca985b4c3b32a8a
Marc Crawford <marc12crawford@gmail.com>
2026-02-04 13:13:02 +0000

    Delete assets /css /script.js

--- assets /css /script.js
+++
@@ -1,9 +0,0 @@
-function confirmWalletAction() {
-    const result = confirm("Would you like to save your password to the wallet?");
-
-    if (result) {
-        alert("You clicked YES");
-    } else {
-        alert("You clicked NO");
-    }
-}
