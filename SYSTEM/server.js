b488b1c26cf7c81e10bd8cd44b49d25262419e43
marcc1875 <mc2088@hw.ac.uk>
2026-02-08 13:34:02 +0000

    Succesfully Implemented Registration

--- SYSTEM/server.js
+++ SYSTEM/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-
+const bcrypt = require("bcrypt");
+const pool = require("./db");
 require("dotenv").config();
 
 const app = express();
@@ -18,7 +19,35 @@ app.listen(3000, () => {
   console.log("Running on http://localhost:3000");
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Form received");
+app.post("/register", async (req, res) => {
+
+  try {
+    const { email, password, repeatPassword } = req.body;
+
+    if (password !== repeatPassword) {
+      return res.send("Passwords do not match");
+    }
+
+    const [existing] = await pool.query(
+      "SELECT id FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (existing.length > 0) {
+      return res.send("Email already registered");
+    }
+
+    await pool.query(
+      "INSERT INTO users (email, password) VALUES (?, ?)",
+      [email, password]
+    );
+
+    res.send("Registration successful");
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
+
+
